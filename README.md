0412f02d4a94742b7bacc7efe5ccb1a40e72f971
John Bampton <jbampton@users.noreply.github.com>
2018-06-18 01:13:44 +1000

    Update README, implement OptionParser and add .rubocop.yml

--- README.md
+++ README.md
@@ -2,19 +2,16 @@
 
 Ruby script to check file checksums with the Windows CertUtil Command
 
-**hashcheck.rb** is a Ruby based command line program that checks file checksums and runs as shown below:
-
+**hashcheck.rb** is a Ruby based command line program that runs as shown below:
 ```
-PS C:\Users\john\Downloads> ruby .\hashcheck.rb .\rubyinstaller-2.3.1-x64.exe SHA256
-a883e24b9a2b383170e17c372cec32bdeb2804ebc1ca5997d68d97cdddae883c
-PS C:\Users\john\Downloads>
+$ ruby hashcheck.rb -h
+Usage: hashcheck.rb [options]
+    -f, --file file                  Path to file
+    -a, --algorithm algorithm        Algorithm
+    -h, --help                       Displays help
+Beast@Destiny~/GitHub/hashcheck $
 ```
-
-**hashcheck.rb** is shown below:
-
-```ruby
-#ARGV[0] is the file you want to check. ARGV[1] is the hash algorithm
-#Secure_Hash_Algorithm choices: MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
-shellcommand = `CertUtil -hashfile "#{ARGV[0]}" "#{ARGV[1]}"`
-puts shellcommand.inspect.split('\n')[1].split.join
+To enter interactive mode type:
+```
+$ ruby hashcheck.rb
 ```
--- hashcheck.rb
+++ hashcheck.rb
@@ -1,5 +1,38 @@
-#ARGV[0] is the file you want to check. ARGV[1] is the hash algorithm
-#Secure_Hash_Algorithm choices: MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
+# Secure_Hash_Algorithm choices: MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
 
-shellcommand = `CertUtil -hashfile "#{ARGV[0]}" "#{ARGV[1]}"`
-puts shellcommand.inspect.split('\n')[1].split.join
+require 'paint'
+require 'optparse'
+
+# implement commandline options
+options = {:file => nil, :algorithm => nil}
+
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{Paint['hashcheck.rb [options]', :red, :white]}"
+
+  opts.on('-f', '--file file', 'Path to file') do |file|
+    options[:file] = file
+  end
+
+  opts.on('-a', '--algorithm algorithm', 'Algorithm') do |algorithm|
+    options[:algorithm] = algorithm
+  end
+
+  opts.on('-h', '--help', 'Displays help') do
+    puts opts
+    exit
+  end
+end
+
+parser.parse!
+
+if options[:file].nil?
+  print 'Enter path to file: '
+  options[:file] = STDIN.gets.chomp
+end
+if options[:algorithm].nil?
+  print 'Enter algorithm: '
+  options[:algorithm] = STDIN.gets.chomp
+end
+
+shell_command = `CertUtil -hashfile "#{options[:file]}" "#{options[:algorithm]}"`
+puts shell_command.inspect.split('\n')[1]
